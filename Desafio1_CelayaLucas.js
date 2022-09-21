0a1c0f7ce631e9a9888afb576c14e523287c1823
lucelaya <lucelaya@gmail.com>
2022-09-21 17:50:34 -0300

    Desafio1

---
+++ Desafio1_CelayaLucas.js
@@ -0,0 +1,42 @@
+class Usuario{
+    constructor(nombre, apellido, libros, mascotas){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.libros = libros;
+        this.mascotas = mascotas;
+    }
+    getFullName(){
+        return `Nombre Completo del Usuario: ${this.nombre} ${this.apellido}`
+    }
+    countMascotas(){
+        return `Cantidad de mascotas: ${this.mascotas.length}`
+    }
+    addBook(nombre, autor){
+        const newBook = {nombre: nombre, autor: autor};
+        // console.log(newBook);
+        this.libros.push(newBook);
+        console.log("Se agrego el libro: " + newBook.nombre)
+    }
+    // getBookNames(){
+    //     console.log("--Lista de Libros del Usuario:")
+    //     this.libros.forEach((libro)=>{console.log(libro.nombre)})
+    //     console.log("--fin lista de libros del usuario")
+    // }
+    //Otra forma de recorrer un array o de filtrar info de un array a otro
+    getBookNames(){
+        let coleccion = this.libros
+        let nombre = [];
+        coleccion.map(libro=>{
+            nombre.push(libro.nombre)
+        })
+        return nombre
+    }
+}
+
+const usuario1 = new Usuario("Lucas", "Celaya", [{nombre:"Retratos", autor:"Pablo Bernasconi"}], ["perro", "gato", "pez"]);
+console.log(usuario1)
+console.log(usuario1.getFullName())
+console.log(usuario1.countMascotas())
+usuario1.addBook("Harry Potter y el Prisionero de Azcaban","J.K Rowling");
+console.log(usuario1.getBookNames())
+console.log(usuario1)
