e8f2d927eb28d67913051477466acd3d5295c650
lucelaya <lucelaya@gmail.com>
2022-10-20 00:47:58 -0300

    Desafio4_ejs

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 import express from 'express'
 import { router } from './src/routes/index.js'
-import { engine } from 'express-handlebars'
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
-//HandleBarS
-app.engine('hbs', engine({ extname: 'hbs' }))
-app.set('view engine', 'hbs')
-app.set('views', './src/views')
+//carpeta Public accecible para el usuario
 app.use(express.static('public'))
+
+//configuramos el motor de plantillas
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
 
 app.use('/', router)
 
-app.listen(8080, () => console.log('Servidor funcionando en el puerto 8080'))
+app.listen(8080,()=>console.log("server listening"));
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,12 @@
 import { Router } from 'express'
-import { Customers } from '../components/Customers.js'
 import { Contenedor } from '../components/Contenedor.js'
 
 const comics = new Contenedor("./src/data/comics.json");
-const customers = new Customers()
 const router = Router()
 
-router.get('/', (req, res) => {
-  res.render('home') 
-})
-
-router.post('/api/customers', (req, res) => {
-  customers.add(req.body)
-  res.redirect('/')
-})
-
-router.get('/customers', (req, res) => {
-  res.render('customers', { customers: customers.getAll() })
+router.get('/', async(req, res) => {
+  const c = await comics.getAll()
+  res.render('home',{ comics: c}) 
 })
 
 router.post('/api/comics', (req, res) => {
