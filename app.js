735f582d5981c6b36d813c0ac0c4d8d334f0c7db
lucelaya <lucelaya@gmail.com>
2022-10-29 20:23:00 -0300

    Desafio7_base

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import { router } from './src/routes/index.js'
 import { engine } from 'express-handlebars'
-import { Server } from 'socket.io'
+// import { Server } from 'socket.io'
 
 import { Contenedor } from './src/components/Contenedor.js'
 const productos = new Contenedor("./src/data/comics.json");
@@ -13,7 +13,7 @@ const app = express()
 const server = app.listen(PORT, ()=>console.log(`listening on port ${PORT}`));
 
 //Servidor de websocket y lo conectamos con el servidor de express
-const io = new Server(server);
+// const io = new Server(server);
 
 //Configuracion: al body lo interpreta como JSON
 app.use(express.json())
@@ -35,28 +35,27 @@ app.engine('hbs', engine({ extname: 'hbs' }))
 app.set('view engine', 'hbs')
 app.set('views', './src/views')
 
-const historicoMensajes = [];
+// const historicoMensajes = [];
+// io.on("connection",async(socket)=>{
+//     console.log("nuevo usuario conectado", socket.id);
+//     //enviar a todos menos al socket conectado
+//     socket.broadcast.emit("newUser");
+//     socket.emit("historico",historicoMensajes)
+//     socket.on("message",data=>{
+//         console.log(data);
+//         historicoMensajes.push(data);
+//         //enviar a todos
+//         io.sockets.emit("historico",historicoMensajes);
+//     })
 
-io.on("connection",async(socket)=>{
-    console.log("nuevo usuario conectado", socket.id);
-    //enviar a todos menos al socket conectado
-    socket.broadcast.emit("newUser");
-    socket.emit("historico",historicoMensajes)
-    socket.on("message",data=>{
-        console.log(data);
-        historicoMensajes.push(data);
-        //enviar a todos
-        io.sockets.emit("historico",historicoMensajes);
-    })
+//     // primer pintada ya realizada en el route
+//     // socket.emit("productos",await productos.getAll())
+//     socket.on("alta",async data=>{
+//         console.log(data);
+//         console.log(productos);
+//         await productos.save(data);
+//         //enviar a todos
+//         io.sockets.emit("productos",await productos.getAll());
+//     })
 
-    // primer pintada ya realizada en el route
-    // socket.emit("productos",await productos.getAll())
-    socket.on("alta",async data=>{
-        console.log(data);
-        console.log(productos);
-        await productos.save(data);
-        //enviar a todos
-        io.sockets.emit("productos",await productos.getAll());
-    })
-
-})
+// })
